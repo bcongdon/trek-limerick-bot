55ed42308f8b92ab93077675c30e6ca70ec581e5
Benjamin Congdon <bencongdon96@gmail.com>
2017-03-24 09:07:16 -0700

    Ability to post tweets; deployment script to lambda

--- trek-limerick-bot.js
+++ trek-limerick-bot.js
@@ -1,14 +1,19 @@
 #!/usr/bin/env node
 'use strict';
 
+const async = require("async");
+const botUtilities = require('bot-utilities');
+const fs = require('fs');
+const glob = require("glob");
+const mkdirp = require('mkdirp');
 const program = require('commander');
 const rhyme = require('rhyme');
-const fs = require('fs');
 const Tokenizer = require('sentence-tokenizer');
-const glob = require("glob");
-const async = require("async");
-const mkdirp = require('mkdirp');
+const Twit = require('twit');
+require('dotenv').config();
 var _ = require('lodash');
+_.mixin(botUtilities.lodashMixins);
+_.mixin(Twit.prototype, botUtilities.twitMixins);
 
 function scriptToSentences(script) {
   var tokenizer = new Tokenizer('script');
@@ -157,19 +162,67 @@ function generateLimerick(data) {
   return [n[0], n[1], s[0], s[1], n[2]]
 }
 
+function splitIntoTweets(lines) {
+  var tweets = [];
+  var curr_line = '';
+  for(var i = 0; i < lines.length; i++) {
+    var str = lines[i].line;
+    if(str.length + curr_line.length >= 139) {
+      tweets.push(curr_line);
+      curr_line = '';
+    }
+    if(curr_line.length > 0)
+      curr_line += '\n'
+    curr_line += str;
+  }
+  if(curr_line.length > 0)
+    tweets.push(curr_line)
+  return tweets;
+}
+
+function getData(cb) {
+  glob("processed/*.json", function(er, files) {
+    var data = {};
+    files.forEach(function(fpath){
+      var newData = JSON.parse(fs.readFileSync(fpath));
+      var oldsum = keyCount(data);
+      var newsum = keyCount(newData)
+      data = _.mergeWith(data, newData, merger);
+    });
+    cb(data);
+  });
+}
+
 program
   .command('generate')
   .description('Generate a limerick')
   .action(function() {
-    glob("processed/*.json", function(er, files) {
-      var data = {};
-      files.forEach(function(fpath){
-        var newData = JSON.parse(fs.readFileSync(fpath));
-        var oldsum = keyCount(data);
-        var newsum = keyCount(newData)
-        data = _.mergeWith(data, newData, merger);
+    getData(function(data) {
+      var limerick = generateLimerick(data)
+      console.log(limerick.map(function(d){return d.line}).join('\n'));
+    });
+  });
+
+program
+  .command('post')
+  .description('Post a limerick as a tweet stream')
+  .action(function() {
+    getData(function(data) {
+      var limerick = generateLimerick(data)
+      var tweets = splitIntoTweets(limerick);
+      var T = new Twit(botUtilities.getTwitterAuthFromEnv());
+      async.reduce(tweets, {}, function(prev, str, cb) {
+        var opts = {
+          in_reply_to_status_id: prev.id_str,
+          status: str
+        }
+        T.post('statuses/update', opts, function(err, data, resp) {
+          cb(err, data);
+        });
+      }, function(err) {
+        if(err) throw err;
+        else console.log("Posted tweets successfully.")
       });
-      console.log(generateLimerick(data).map(function(d){return d.line}).join('\n'));
     });
   });
 
